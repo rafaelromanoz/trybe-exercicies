c41db86e7743fe0dfc1169b3a8e3cbc949cb4f39
Rafael Romano <rafa-kun@hotmail.com>
2021-05-19 16:35:28 -0300

    Conteúdo do dia 4.1

---
+++ Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia-01-introducao/parte5.js
@@ -0,0 +1,18 @@
+let status='ta fodida'
+
+switch(status){
+    case 'aprovada':
+        console.log("Você está aprovada!")
+        break;
+
+        case 'lista':
+            console.log("Você está na lista de espera")
+                break;
+            case 'reprovada':
+                console.log("Você está reprovada")
+                break;
+
+                default:
+                    console.log("Não se aplica")
+
+}
