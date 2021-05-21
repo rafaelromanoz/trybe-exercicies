e8bb654e760c329148e1bb6fdadb7ba3e3ef605e
Rafael Romano <rafa-kun@hotmail.com>
2021-05-21 00:02:23 -0300

    adicionando exercicio 3

--- Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia-02-forarray/exercicio3.js
+++ Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia-02-forarray/exercicio3.js
@@ -1,1 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma=0;
+
+for (contador =0;contador<numbers.length;contador+=1){
+    soma +=numbers[contador]
+    
+}
+let media=soma/numbers.length
+
+console.log(media)
+
