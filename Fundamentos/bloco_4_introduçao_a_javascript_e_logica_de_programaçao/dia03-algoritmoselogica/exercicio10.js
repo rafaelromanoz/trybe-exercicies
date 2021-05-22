ab1ecaf214b45abe16bcf1658367db67d799b768
Rafael Romano <rafa-kun@hotmail.com>
2021-05-22 10:54:56 -0300

    Nao consegui fazer os exercicios do dia somente conteudo

--- Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia03-algoritmoselogica/exercicio10.js
+++ Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia03-algoritmoselogica/exercicio10.js
@@ -1,5 +1,10 @@
-let n=8;
+let n = 5;
+let symbol = '*';
+let inputLine = '';
 
-for(let index=0;index<=n;index+=1){
-    console.log(n*'*')
-}
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
