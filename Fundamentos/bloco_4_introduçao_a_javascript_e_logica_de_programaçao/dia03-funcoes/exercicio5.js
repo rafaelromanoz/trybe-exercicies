5243f93d730c245c6abc3a8f8025c3728deb4591
Rafael Romano <rafa-kun@hotmail.com>
2021-05-26 15:01:57 -0300

    adicionando alguma coisa

--- Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia03-funcoes/exercicio5.js
+++ Fundamentos/bloco_4_introduçao_a_javascript_e_logica_de_programaçao/dia03-funcoes/exercicio5.js
@@ -1,15 +1,6 @@
-//Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
-
-function inteiroMaisRepete(array) {
-  let numberOfRepetitions=0;
-  let contador=array[0];
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] === contador) {
-      numberOfRepetitions++
-      console.log(numberOfRepetitions)
-    }
-  }
-
+// Desafio 7
+function catAndMouse(mouse,cat1,cat2) {
+  
 }
 
-inteiroMaisRepete((array = [2, 3, 2, 5, 8, 2, 4, 4, 4, 4]));
+console.log(catAndMouse(0,2,3))
