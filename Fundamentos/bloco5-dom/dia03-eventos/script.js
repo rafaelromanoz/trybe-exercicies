4f6f378397c615835aa666a8cf8374dc1a986012
Rafael Romano <rafa-kun@hotmail.com>
2021-06-01 08:31:54 -0300

    Adicionando exercicios até o 10

--- Fundamentos/bloco5-dom/dia03-eventos/script.js
+++ Fundamentos/bloco5-dom/dia03-eventos/script.js
@@ -40,7 +40,8 @@ function createDaysOfCalendar() {
       dezDaysList[index2] === 31
     ) {
       daysOfWeek.classList.add("holiday");
-    } else if (
+    }
+    if (
       dezDaysList[index2] === 4 ||
       dezDaysList[index2] === 11 ||
       dezDaysList[index2] === 18 ||
@@ -50,33 +51,139 @@ function createDaysOfCalendar() {
     }
   }
 }
-const button = document.querySelector(".buttons-container");
+createDaysOfCalendar();
 
-function createButton(Feriados) {
+//criando botão do feriado
+function createButton() {
+  const button = document.querySelector(".buttons-container");
   const buttonAdd = document.createElement("button");
-  buttonAdd.innerText = "Feriados";
-  button.appendChild(buttonAdd);
+  buttonAdd.innerHTML = "Feriados";
   buttonAdd.id = "btn-holiday";
+  button.appendChild(buttonAdd);
 }
-
-function backgroundBtt() {
-  let alteraCor = document.querySelectorAll(".holiday");
-
-  for (let key in alteraCor) alteraCor[key].style.backgroundColor = "red";
-}
-
-function dayFriday() {
-  const buttonAdd2 = document.createElement("button");
-  buttonAdd2.innerText = "Sexta-Feira";
-  button.appendChild(buttonAdd2);
-  buttonAdd2.className = "btn-friday";
-}
-
-function back
-button.addEventListener("click", backgroundBtt);
-
-dayFriday();
 
 createButton();
+// fazendo exercicio 5 de colocar fundo nos feriados
+const holidayButtonClick = document.querySelector("#btn-holiday");
 
-createDaysOfCalendar();
+let clickHoliday = false;
+
+holidayButtonClick.addEventListener("click", function () {
+  const li = document.getElementsByClassName("holiday");
+
+  if (clickHoliday == false) {
+    for (let index = 0; index < li.length; index += 1) {
+      li[index].style.backgroundColor = "red";
+    }
+    clickHoliday = true;
+    return void 0;
+  }
+  if (clickHoliday == true) {
+    for (let index = 0; index < li.length; index += 1) {
+      li[index].style.backgroundColor = "rgb(238,238,238)";
+    }
+    clickHoliday = false;
+  }
+});
+// criando botão sexta feira
+function dayFriday() {
+  const button = document.querySelector(".buttons-container");
+  const buttonAdd2 = document.createElement("button");
+  buttonAdd2.innerHTML = "Sexta-Feira";
+  buttonAdd2.id = "btn-friday";
+  button.appendChild(buttonAdd2);
+}
+dayFriday();
+
+// exercicio 6 alterando friday
+const fridayButtonClick = document.querySelector("#btn-friday");
+
+fridayButtonClick.addEventListener("click", function () {
+  const li = document.getElementsByClassName("friday");
+
+  if (clickHoliday == false) {
+    for (let index = 0; index < li.length; index += 1) {
+      li[index].innerText = "SEXTOU!";
+    }
+    clickHoliday = true;
+    return void 0;
+  }
+  if (clickHoliday == true) {
+    li[0].innerText = "4";
+    li[1].innerText = "11";
+    li[2].innerText = "18";
+    li[3].innerText = "25";
+  }
+  clickHoliday = false;
+});
+// exercicio 6 zoom nas letras
+
+let dias = document.getElementById("days");
+
+dias.addEventListener("mouseover", function (event) {
+  event.target.style.fontSize = "26px";
+});
+dias.addEventListener("mouseout", function (event) {
+  event.target.style.fontSize = "18px";
+});
+
+/* Exercicio 7 Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .*/
+
+function addTask() {
+  let tasks = document.querySelector(".my-tasks");
+  const spanTask = document.createElement("span");
+  spanTask.innerText = "Cozinhar:";
+  spanTask.className = "my-tasks";
+  tasks.appendChild(spanTask);
+}
+
+addTask();
+//exercicio 8 criando a div
+function addDiv() {
+  let colorTasks = document.querySelector(".my-tasks");
+  const divTask = document.createElement("div");
+  divTask.style.backgroundColor = "green";
+  divTask.className = "task";
+  colorTasks.appendChild(divTask);
+}
+addDiv();
+
+//exercicio 9 fazendo função de adicionar e remover classe selected clicando;
+
+function setTaskClass() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let myTasks = document.querySelector('.task');
+
+  myTasks.addEventListener('click', function(event) {
+    if (selectedTask.length === 0) {
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  });
+};
+
+setTaskClass();
+
+
+//exercicio 10 marcando li
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
+
+setDayColor();
+
