3c6e3d99b110e9dbdb1a82ae8041c4dbcbea087c
Rafael Romano <rafa-kun@hotmail.com>
2021-06-09 18:50:32 -0300

    adicionando exercicio 1

---
+++ Fundamentos/bloco6-flexbox/script.js
@@ -0,0 +1,44 @@
+const selected = document.querySelector("#selected");
+
+const states  = [
+    { value: 'AC', label: 'Acre' },
+    { value: 'AL', label: 'Alagoas' },
+    { value: 'AP', label: 'Amapá' },
+    { value: 'AM', label: 'Amazonas' },
+    { value: 'BA', label: 'Bahia' },
+    { value: 'CE', label: 'Ceará' },
+    { value: 'DF', label: 'Distrito Federal' },
+    { value: 'ES', label: 'Espírito Santo' },
+    { value: 'GO', label: 'Goías' },
+    { value: 'MA', label: 'Maranhão' },
+    { value: 'MT', label: 'Mato Grosso' },
+    { value: 'MS', label: 'Mato Grosso do Sul' },
+    { value: 'MG', label: 'Minas Gerais' },
+    { value: 'PA', label: 'Pará' },
+    { value: 'PB', label: 'Paraíba' },
+    { value: 'PR', label: 'Paraná' },
+    { value: 'PE', label: 'Pernambuco' },
+    { value: 'PI', label: 'Piauí' },
+    { value: 'RJ', label: 'Rio de Janeiro' },
+    { value: 'RN', label: 'Rio Grande do Norte' },
+    { value: 'RS', label: 'Rio Grande do Sul' },
+    { value: 'RO', label: 'Rondônia' },
+    { value: 'RR', label: 'Roraíma' },
+    { value: 'SC', label: 'Santa Catarina' },
+    { value: 'SP', label: 'São Paulo' },
+    { value: 'SE', label: 'Sergipe' },
+    { value: 'TO', label: 'Tocantins' },
+    ];
+
+
+function createOptions() {
+  for (let index in states) {
+    let option = document.createElement("option");
+    console.log(states[index].label)
+    option.innerText = states[index].label;
+    option.value=states[index].value;
+    selected.appendChild(option);
+  }
+}
+
+createOptions();
