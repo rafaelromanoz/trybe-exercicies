9e68493cdb6bb051a2ba4b48abece8f4146a1194
Rafael Romano <rafa-kun@hotmail.com>
2021-06-18 11:24:03 -0300

    Adicionando exercicios do dia 7.1

--- Fundamentos/bloco7-js&testesunitários/parte1.js
+++ Fundamentos/bloco7-js&testesunitários/parte1.js
@@ -5,35 +5,30 @@
 // Modifique as concatenações para template literals .
 // Copie o código abaixo.
 
-
-
-const testingScope =(escopo) => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      const  elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    } 
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
   }
+};
 
-  testingScope(true);
+testingScope(true);
 
 //   Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 // Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
 // Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
 // Copie o código abaixo Exercicio 2 e bonus.
 
-
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const sortFunction=(a,b)=>(a-b);
+const sortFunction = (a, b) => a - b;
 
+oddsAndEvens.sort(sortFunction, oddsAndEvens);
 
-oddsAndEvens.sort (sortFunction,oddsAndEvens)
-
-console.log(`Os números fornecidos encontram se em ordem crescente ${oddsAndEvens}`)
-
-
-
+console.log(
+  `Os números fornecidos encontram se em ordem crescente ${oddsAndEvens}`
+);
--- Fundamentos/bloco7-js&testesunitários/parte2.js
+++ Fundamentos/bloco7-js&testesunitários/parte2.js
@@ -2,6 +2,25 @@
 // Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
 
-const calculoFatorial= (number) => number===0 ? 1 : number*calculoFatorial(number-1)
+const calculoFatorial = (number) =>
+  number === 0 ? 1 : number * calculoFatorial(number - 1);
 
 console.log(calculoFatorial(6));
+
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+// Exemplo:
+
+const verificaPalavra = (palavra) => {
+  const palavras = palavra.split(' ');
+  let palavraMaior = palavras[0];
+  for (let index = 0; index < palavras.length; index += 1) {
+    if (palavras[index].length > palavraMaior.length) {
+      palavraMaior = palavras[index];
+    }
+  }
+  return palavraMaior;
+};
+
+console.log(
+  verificaPalavra('Antônio foi no banheiros e não sabemos o que aconteceu')
+);
