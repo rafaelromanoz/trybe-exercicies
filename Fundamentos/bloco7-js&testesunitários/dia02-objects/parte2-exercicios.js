f56de13ef9e0af232989dc0abc0e716b4c1632c6
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-06-19 10:04:02 -0300

    adicionando exercicios parte 1

---
+++ Fundamentos/bloco7-js&testesunitários/dia02-objects/parte2-exercicios.js
@@ -0,0 +1,28 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const adicionarTurnoManha = (obj,chave,value)=>{
+  obj[chave]=value;
+  return obj;
+}
+
+adicionarTurnoManha(lesson2,'turno','manhã');
+
+const mostrarKeys=(obj)
--- Fundamentos/bloco7-js&testesunitários/dia02-objects/parte4-object-entries.js
+++ Fundamentos/bloco7-js&testesunitários/dia02-objects/parte4-object-entries.js
@@ -7,8 +7,8 @@ const países = {
 const pairKeyValue = Object.entries(países);
 console.log(pairKeyValue);
 
-for(index in pairKeyValue) {
+for (index in pairKeyValue) {
   console.log('--------');
   console.log('País:', pairKeyValue[index][0]);
   console.log('Capital:', pairKeyValue[index][1]);
-};
+}
