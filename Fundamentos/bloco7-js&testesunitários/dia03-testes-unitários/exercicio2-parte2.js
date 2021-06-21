2aaf7f7f97160f8f15488c518a99c6bd642089bb
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-06-21 19:50:33 -0300

    adicionando exercicio2-parte2

---
+++ Fundamentos/bloco7-js&testesunitários/dia03-testes-unitários/exercicio2-parte2.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+// escreva a função wordLengths aqui
+const wordLengths = (arrWord) => {
+  let lenght=[];
+  for(let index in arrWord){
+    lenght.push(arrWord[index].length)
+  }
+  return lenght
+}
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+console.log(wordLengths(words))
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
