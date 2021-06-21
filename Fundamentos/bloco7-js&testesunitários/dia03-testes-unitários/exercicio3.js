ab68d097a563ef69959cf604d0689ea726afad66
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-06-21 19:12:46 -0300

    Adicionando exercicio 4

--- Fundamentos/bloco7-js&testesunitários/dia03-testes-unitários/exercicio3.js
+++ Fundamentos/bloco7-js&testesunitários/dia03-testes-unitários/exercicio3.js
@@ -17,6 +17,12 @@ assert.deepStrictEqual(myRemoveWithoutCopy([1,2,3,4],3),[1,2,4]);
 
 assert.notDeepStrictEqual(myRemoveWithoutCopy([1,2,3,4],3),[1,2,3,4]);
 
+assert.deepStrictEqual(myRemoveWithoutCopy([1,2,3,4],5),[1,2,3,4]);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1,2,3,4],5),[1,2,3,4]);
+
+
+
 
 
 
