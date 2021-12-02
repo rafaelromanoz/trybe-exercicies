cf4c972bb790a1ef12bbcff810d576981b8567f3
Rafael Romano <rafa-kun@hotmail.com>
2021-12-02 13:57:37 -0300

    :zap: adicionando algumas coisas
    vqv

--- Desenvolvimento Backend/Bloco-26-NodeJS/dia-03-testesNodejs/exercicios4e5/exercicio4e5func.js
+++ Desenvolvimento Backend/Bloco-26-NodeJS/dia-03-testesNodejs/exercicios4e5/exercicio4e5func.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const escritaArquivo = (nomearquivo, conteudo) => {
   fs.writeFileSync(`${nomearquivo}`, conteudo, 'utf8');
   return 'Deu certo pae';
